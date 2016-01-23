3b32f3173a49a05c66debd2e64c414fbe0584111
Terry Wen <wen.terry.e@gmail.com>
2016-01-23 15:30:18 -0700

    slight edits to test UI

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -14,7 +14,7 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
 var client_id = 'efb4f5e964e94d2aa3d5c7fa1fffabe0'; // Your client id
-var client_secret = ; // Your client secret
+var client_secret = '2b6bd34ae2164fdeb46349c9a9ab43b0'; // Your client secret
 var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
