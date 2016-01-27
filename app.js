7eb1d7b81de0262fc6956f9c7ef3c0025fa10fd9
Terry Wen <wen.terry.e@gmail.com>
2016-01-27 11:44:46 -0700

    attempt number 2

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
 var client_id = 'efb4f5e964e94d2aa3d5c7fa1fffabe0'; // Your client id
-var client_secret = ''; // Your client secret
+var client_secret = '2b6bd34ae2164fdeb46349c9a9ab43b0'; // Your client secret
 var redirect_uri = 'https://concertfeed.herokuapp.com/callback'; // Your redirect uri
 
 /**
@@ -42,8 +42,8 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')))
    .use(cookieParser());
 
-app.set('port', (process.env.PORT || 5000));
-//For avoidong Heroku $PORT error
+//app.set('port', (process.env.PORT || 5000));
+//For avoiding Heroku $PORT error
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.render('index', {});
