6db06a0272200f80a310cd78028318e359a8c8c3
Terry Wen <wen.terry.e@gmail.com>
2016-01-27 11:37:55 -0700

    idk whats going on

--- app.js
+++ app.js
@@ -42,14 +42,14 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')))
    .use(cookieParser());
 
-/*app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
     var result = 'App is running'
     response.render('index', {});
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
-});*/
+});
    
 app.get('/login', function(req, res) {
 
